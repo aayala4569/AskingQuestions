4b7ef375e5c9abe3df149e0c9316bd2e30085f11
aayala4569 <angelicaayala100@gmail.com>
2022-10-24 14:51:26 -0600

    Asking Questions Repo.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Angelica Ayala
+//Date Revised:10/24/2022
+//Mini Challenge # 3 - Asking Questions
+//-------Description---------------------------------------
+//Declared a data type and variable
+//oupt a question asking "What is your name?"
+//requested a user input
+//output a question asking"What time did you wake up?"
+//created another input that turse user input (string) into an interger
+//Result: What is your name and what time did you wake up?
+
+//Peer Reviewed by: 
+//Date:
+//Review:
+
+string Name;
+string wakeTim;
+
+Console.WriteLine("What is your name?");
+
+Name = Console.ReadLine();
+
+Console.WriteLine("What time did you wake up?");
+
+wakeTim = Console.ReadLine();
+
+Console.WriteLine($"My name is {Name} and I woke up at {wakeTim}.");
